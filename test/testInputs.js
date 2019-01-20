f176844080782f1e29e3ba7907f8b195a03fc517
Balasubramanian Kasamuthu <kbala@Balasubramanians-MacBook-Pro.local>
2019-01-20 19:43:13 +0530

    updated

--- test/testInputs.js
+++ test/testInputs.js
@@ -4,15 +4,15 @@ var tap = require('tap')
 var invnum = require('../index')
 
 tap.throws(
-    function () {
-      invnum.next()
-    }, {}, { message: 'Missing required param: invoiceNumber' },
-    'invoiceNumber cannot be empty'
+  function () {
+    invnum.next()
+  }, {}, { message: 'Missing required param: invoiceNumber' },
+  'invoiceNumber cannot be empty'
 )
 
 tap.throws(
-    function () {
-      invnum.next('ABC/2017/')
-    }, {}, { message: 'invoiceNumber cannot end with illegal character' },
-    'str cannot be empty'
+  function () {
+    invnum.next('ABC/2017/')
+  }, {}, { message: 'invoiceNumber cannot end with illegal character' },
+  'str cannot be empty'
 )
